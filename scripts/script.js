795018ef149e6d07fa604374dff24775c32e36b5
OLambur04 <obee_lamburc31@unicah.edu>
2026-01-24 17:40:48 -0600

    Mi Portafolio
    Realización de Mi Portafolio utilizando HTML, CSS y JavaScript.

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', function() {
+        alert('Gracias por tu interés, pronto me pondré en contacto contigo.');
+    });
+});
