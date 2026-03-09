475872670f4c170f8dbbaf072ba7a0e67fb47b16
OLambur04 <obee_lamburc31@unicah.edu>
2026-03-08 23:07:04 -0600

    Proyecto con Vite y LESS

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', function() {
-        alert('Gracias por tu interés, pronto me pondré en contacto contigo.');
-    });
-});
