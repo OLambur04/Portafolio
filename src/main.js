2d215dfe5ce641ce507f0d3a990a9c1afa48b8c9
OLambur04 <obee_lamburc31@unicah.edu>
2026-03-09 22:43:15 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,8 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
         })
     })
 })
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', function() {
-        alert('Gracias por tu interés, pronto me pondré en contacto contigo.');
-    });
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', function () {
+    alert('Gracias por tu interés, pronto me pondré en contacto contigo.');
 });
